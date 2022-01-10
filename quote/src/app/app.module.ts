5d394fe6028e28139c901387798e8adfb33b7063
dante <maxgamerdu@gmail.com>
2022-01-10 23:14:59 +0300

    added extra components and classes as build up on the structure

--- quote/src/app/app.module.ts
+++ quote/src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../app/app.component';
+import { FormComponent } from '../app/form/form.component';
+import { DetailsComponent } from './details/details.component';
 import { QuoteComponent } from './quote/quote.component';
+import { Keys } from './keys';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    FormComponent,
+    DetailsComponent,
+    QuoteComponent,
+    Keys
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- quote/src/app/quote/quote.component.spec.ts
+++ quote/src/app/quote/quote.component.spec.ts
@@ -1,25 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { QuoteComponent } from './quote.component';
-
-describe('QuoteComponent', () => {
-  let component: QuoteComponent;
-  let fixture: ComponentFixture<QuoteComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ QuoteComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(QuoteComponent);
-    component = fixture.componentInstance;
+import { TestBed, waitForAsync,} from '@angular/core/testing';
+import { AppComponent } from '../app.component';
+describe('AppComponent', () => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+  it('create the app', waitForAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+  it(`title 'app'`, waitForAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app');
+  }));
+  it('title in a h1 tag', waitForAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to this app!');
+  }));
 });
